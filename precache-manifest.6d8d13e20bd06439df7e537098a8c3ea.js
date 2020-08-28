a42abf235e6821489fa4c6586ba49b0b22e5daf6
Leonid Zychkov <buldog52@gmail.com>
2020-08-28 23:07:15 +0300

    Updates

---
+++ precache-manifest.6d8d13e20bd06439df7e537098a8c3ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6883b5941d6e1fe6875971d6dffa43a6",
+    "url": "/react-contact-list/index.html"
+  },
+  {
+    "revision": "e8a6ac7150a633bc94b5",
+    "url": "/react-contact-list/static/css/main.e7520c7f.chunk.css"
+  },
+  {
+    "revision": "ad7e406678820caa21be",
+    "url": "/react-contact-list/static/js/2.3b28baca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-contact-list/static/js/2.3b28baca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a6ac7150a633bc94b5",
+    "url": "/react-contact-list/static/js/main.b49026e6.chunk.js"
+  },
+  {
+    "revision": "251b9413db6772d06c79",
+    "url": "/react-contact-list/static/js/runtime-main.0d0f1908.js"
+  }
+]);
